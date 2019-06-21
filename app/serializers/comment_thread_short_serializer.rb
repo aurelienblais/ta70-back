345ddd5617c1954d9c7759c8b4c8a9c7c070f7c7
Aurélien <aurelien.blais@whataniceplace.com>
2019-06-21 15:31:00 +0200

    Add comments count on POI

--- app/serializers/comment_thread_short_serializer.rb
+++ app/serializers/comment_thread_short_serializer.rb
@@ -6,4 +6,8 @@ class CommentThreadShortSerializer
   attribute :note do |comment_thread|
     comment_thread.comments.average(:note)
   end
+
+  attribute :comments do |comment_thread|
+    comment_thread.comments.length
+  end
 end
