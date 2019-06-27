13b271c5bc1e7a33314662a8182998d319cf8b80
Aurélien <aurelien.blais@whataniceplace.com>
2019-06-27 15:39:31 +0200

    Add lat and lng to crawls

--- app/serializers/crawl_poi_serializer.rb
+++ app/serializers/crawl_poi_serializer.rb
@@ -6,7 +6,9 @@ class CrawlPoiSerializer
   attributes :poi do |crawl_poi|
     {
       id: crawl_poi.poi.id,
-      name: crawl_poi.poi.name
+      name: crawl_poi.poi.name,
+      latitude: crawl_poi.poi.lat,
+      longitude: crawl_poi.poi.lng
     }
   end
 end
