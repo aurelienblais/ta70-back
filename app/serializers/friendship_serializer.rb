cd18aa2290470bad423480c749112ce68e9f6b91
Aurélien <aurelien.blais@whataniceplace.com>
2019-06-27 10:53:50 +0200

    Add id to friendship serializer

--- app/serializers/friendship_serializer.rb
+++ app/serializers/friendship_serializer.rb
@@ -6,10 +6,12 @@ class FriendshipSerializer
   attributes :friend do |friendship, params|
     if params[:current_user] == friendship.user
       {
+        id: friendship.friend.id,
         name: friendship.friend.name
       }
     else
       {
+        id: friendship.user.id,
         name: friendship.user.name
       }
     end
