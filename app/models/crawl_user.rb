7820e7a38d98f92b49c63a638130195e561e9ead
Aurélien <aurelien.blais@whataniceplace.com>
2019-06-27 10:56:56 +0200

    Force crawluser to have accepted status

--- app/models/crawl_user.rb
+++ app/models/crawl_user.rb
@@ -3,4 +3,12 @@
 class CrawlUser < ApplicationRecord
   belongs_to :user
   belongs_to :crawl
+
+  before_save :set_accepted # TODO: fix that, front can't handle invitation so we force them
+
+  private
+
+  def set_accepted
+    self.accepted = true
+  end
 end
